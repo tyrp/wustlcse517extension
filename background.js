6973d74b202aef1fbc4403f1d7fb8de73a6a7631
Anthony Tyrpin <atyrpin@gmail.com>
2014-03-26 12:15:58 -0500

    cleaned up code and fixed error when sending blank input

--- background.js
+++ background.js
@@ -1,17 +1,12 @@
-// Array to hold callback functions
-//var callbacks = [];
-
 // This function is called onload in the popup code
 function getPageInfo(name) {
-    // Add the callback to the queue
-	alert("first" + name);
+	//alert("first" + name);
 	chrome.tabs.executeScript(null,{file:"jquery.min.js"});
-	alert("second" + name);
+	//alert("second" + name);
 	chrome.tabs.executeScript(null,{code:'var data = {name:'+ JSON.stringify(name) +'};'}, function(){
 		chrome.tabs.executeScript(null,{file:"script.js"}, function(){
-			chrome.tabs.sendRequest(tabId, {scriptOptions: {param1:'value1',param2:'value2'}}, function(){
-            //all injected
-			});
+			//all injected
+			chrome.tabs.remove();
 		});
 	});
 
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-$('tr:has(td:contains('+JSON.stringify(data.name)+'))').not(':first').children().each(function(){
-		$(this).css("background-color", "white")
-	});
+if(data.name){
+	$('tr:has(td:contains('+JSON.stringify(data.name)+'))').not(':first').children().each(function(){
+			$(this).css("background-color", "white")
+		});
+}
